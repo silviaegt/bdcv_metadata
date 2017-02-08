22386df72b3c852431b0569fc438b875eec89e03
Oscar <oscard241@gmail.com>
2017-02-07 19:13:04 -0600

    Haciendo pruebas con los términos aceptados

--- prueba.py
+++ prueba.py
@@ -6,12 +6,38 @@ Created on Tue Jan 31 21:28:21 2017
 """
 
 from csv import reader
+from collections import defaultdict,Counter
+from Fingerprint import Fingerprint
 
-doc = list(reader(open('prueba.csv','r',encoding="iso-8859-1"), delimiter=';'))
-firstFilter = set()
+
+doc = list(reader(open('términos_aceptados.txt','r',encoding="iso-8859-1"), delimiter='\t'))
+elements = set()
+cnter = Counter()
 
 for i in doc:
-    if len(i)>0:
-        firstFilter.add(i[0])
+    for j in i:
+        if len(j)>0:
+            cnter[j] +=1
+            elements.add(j)
         
-print(len(firstFilter))
+elements = list(elements)
+elements.sort()
+
+keys = defaultdict(set)
+fp = Fingerprint()
+
+count = 0
+for w in elements:
+    keys[fp.key(w)].add(count)
+    count +=1
+
+for w in keys:
+    num = 0
+    for k in keys[w]:
+        tmp = cnter[elements[k]]
+        print(elements[k])
+        num += tmp
+    if(num > 1):
+        print("total: "+str(num))
+
+#Nota, pensar cómo usar k medias
