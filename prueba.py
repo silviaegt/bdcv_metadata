0a3509c0a9241110af592efd26d3c9392d8e1f5a
Silvia Gutiérrez <silviaegt@gmail.com>
2017-02-17 12:51:24 -0600

    Update prueba.py

--- prueba.py
+++ prueba.py
@@ -14,16 +14,23 @@ from Fingerprint import Fingerprint
         tabuladores,de esta forma se evita problemas con las comas dentro 
         del texto
 """
+
+#1. Leer el archivo con la lista de los términos aceptados
 doc = list(reader(open('términos_aceptados.txt','r',encoding="iso-8859-1"), delimiter='\t'))
+
+#2. Crear una tabla hash con todas las ocurrencias únicas a través de filas y colummnas
+
 elements = set()
+
+#3. Crear un diccionario con el número de ocurrencias por término
 cnter = Counter()
+
+
+#4. For-loop para crear 2 y 3
 """
 doc = Todo el documento
-i = Renglon
-j = Palabra
-elements = Términos aceptados eliminando dúplicados
-keys = un diccionario cuyas llaves son calculadas en fp.key() y en el cual
-se van a hacer los clusters de los términos aceptados
+i = Fila
+j = Celda
 """
 
 for i in doc:
@@ -31,18 +38,28 @@ for i in doc:
         if len(j)>0:
             cnter[j] +=1
             elements.add(j)
-        
+
+#5. elements = Términos aceptados eliminando duplicados y ordenados alfabéticamente
+                
 elements = list(elements)
 elements.sort()
+
+
+
 
 keys = defaultdict(set)
 fp = Fingerprint()
 
+#6. For-loop para alimentar keys donde las llaves son los términos fingerprinteados (Ciudad de México = ciudad de mexico) 
+#y su contenido sus índices (es decir en qué posición ocurren)
 count = 0
 for w in elements:
     keys[fp.key(w)].add(count)
     count +=1
 
+#7. For-loop para saber el tamaño del cluster y guardarlo en csize(ciudad de mexico: 1, 6, 9 -> 3)
+#identificar el tamaño máximo de clúster (maxE)
+#report = tamaño de cluster, termino 1, número de ocurrencias, término 2, número ocurrencias, etc
 report = []
 maxE = 0
 for w in keys:
@@ -59,11 +76,13 @@ for w in keys:
 
 report.sort()
 report.reverse()
-
+#8. Generar el encabezdo de la tabla
 header = ["Tamaño del cluster"]
 for i in range(0,maxE):
     header.append("Palabra")
     header.append("Número de apariciones")
+
+#9. Crear tabla
 
 file = open("reporte.csv",'w',encoding="iso-8859-1")
 dialect = excel
